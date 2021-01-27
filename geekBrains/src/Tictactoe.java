8207ff4218dea55f317675bfe71b9e6befabf22f
Батаева Елена Валентиновна <EBataeva@stream.ru>
2021-01-27 18:39:01 +0300

    домашнее задание 4 - только доработка ходов. поиск вертикалей и диагоналей. пока что

---
+++ geekBrains/src/Tictactoe.java
@@ -0,0 +1,140 @@
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Tictactoe {
+    public static int SIZE = 6;
+    public static int DOTS_TO_WIN = 4;
+    public static final char DOT_EMPTY = '•';
+    public static final char DOT_X = 'X';
+    public static final char DOT_O = 'O';
+    public static char[][] map;
+    public static Scanner sc = new Scanner(System.in);
+    public static Random rand = new Random();
+
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Победил человек");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Победил Искуственный Интеллект");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+        }
+        System.out.println("Игра закончена");
+    }
+
+    public static boolean checkWin(char symb) {
+        boolean cols;
+        boolean rows;
+        boolean doesWin = false;
+
+        for (int x = 0; x < SIZE; x++) {
+            cols = true;
+            rows = true;
+            for (int y = 0; y < SIZE; y++) {
+
+                cols &= (map[x][y] == symb); //проверяем, что для каждого х (то есть в столбце)есть проверяемый знак и логически умножаем
+                rows &= (map[y][x] == symb);//проверяем, что для каждого у (то есть в строке)есть проверяемый знак и логически умножаем
+
+            }
+            if (cols || rows || checkDiagonal(symb)) doesWin = true;
+        }
+        return doesWin;
+    }
+
+    public static boolean checkDiagonal(char symb) {
+        boolean diagonalMajor;
+        boolean diagonalMinor;
+            diagonalMajor = true;
+            diagonalMinor = true;
+            for (int y = 0; y < SIZE; y++) {
+
+                    diagonalMajor &= (map[y][y] == symb);
+                    diagonalMinor &= (map[y][SIZE - y - 1] == symb);
+                }
+
+                if(diagonalMajor || diagonalMinor) return true;
+
+
+        return false;
+    }
+
+    public static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) return false;
+            }
+        }
+        return true;
+    }
+
+    public static void aiTurn() {
+        int x, y;
+        do {
+            x = rand.nextInt(SIZE);
+            y = rand.nextInt(SIZE);
+        } while (!isCellValid(x, y));
+        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
+        map[y][x] = DOT_O;
+    }
+
+    public static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты в формате X Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
+        map[y][x] = DOT_X;
+    }
+
+    public static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+        if (map[y][x] == DOT_EMPTY) return true;
+        return false;
+    }
+
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+    public static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
+
+
